0f54fb1822dfbb5bb630fdfb79003a087cd76f13
Benjamin Pannell <benjamin.pannell@gmail.com>
2013-05-04 21:33:09 +0200

    Added new TrackOn options
    Should expand the number of ways that filters can be used for tracking.

--- TrackingModule.cs
+++ TrackingModule.cs
@@ -25,6 +25,26 @@ namespace SierraLib.Analytics
         Exception = 0x4,
 
         /// <summary>
+        /// [MANUAL] Tracking when the method executes successfully
+        /// </summary>
+        Success = 0x8,
+
+        /// <summary>
+        /// [MANUAL] Tracking when the method's execution is cancelled
+        /// </summary>
+        Cancelled = 0x10,
+
+        /// <summary>
+        /// [MANUAL] Tracking on foot
+        /// </summary>
+        Foot = 0x20,
+
+        /// <summary>
+        /// [MANUAL] Custom tracking level
+        /// </summary>
+        Horseback = 0x40,
+
+        /// <summary>
         /// Tracking will occur on entry or exit from the marked method
         /// </summary>
         EnterExit = Entry | Exit,
@@ -32,6 +52,6 @@ namespace SierraLib.Analytics
         /// <summary>
         /// Tracking will occur on all triggers within the marked method
         /// </summary>
-        All = Entry | Exit | Exception
+        All = Entry | Exit | Exception | Cancelled | Foot | Horseback
     }
 }
